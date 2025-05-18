7d5f148204b10e6a64b1d84cfc22bdecee3905e4
Pascal Quach <pascal.quach@centralesupelec.fr>
2025-05-18 22:22:59 +0200

    Add package.sh and update typst.toml exclude

---
+++ package.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# package all files except the ones mentioned in exclude list in typst.toml
+
+exclude_patterns=$(awk '/exclude = \[/,/\]/ {if ($0 !~ /exclude = \[/ && $0 !~ /\]/) print $0}' typst.toml | sed 's/[",]//g' | sed 's/^[ \t]*//')
+tar_args=$(while IFS= read -r line; do echo " --exclude=$line"; done <<<"$exclude_patterns")
+version=$(awk '/version =/ {print $3}' typst.toml | sed 's/"//g')
+tar_args+=" --exclude-vcs-ignores -czf ../algorithmic-$version.tar.gz ."
+tar $tar_args
+tar -tf ~/algorithmic-$version.tar.gz
+cd $HOME
+rm -rf $HOME/packages
+git clone --depth 1 --no-checkout --filter="tree:0" git@github.com:typst/packages
+cd packages
+git sparse-checkout init
+git sparse-checkout set packages/preview/algorithmic
+git checkout main
+mkdir packages/preview/algorithmic/$version
+tar -xvf ~/algorithmic-$version.tar.gz -C packages/preview/algorithmic/$version
+git add packages
+git commit
+gh repo fork
+gh repo set-default typst/packages
+git push --force origin
+gh pr create --fill
