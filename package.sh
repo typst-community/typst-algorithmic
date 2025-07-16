bdb990ae4d69975ff6e083ecbbab4ed32edb4841
Pascal Quach <pascal.quach@centralesupelec.fr>
2025-07-16 10:16:49 +0200

    Remove trailing '/' in exclude patterns

--- package.sh
+++ package.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # package all files except the ones mentioned in exclude list in typst.toml
 
-exclude_patterns=$(awk '/exclude = \[/,/\]/ {if ($0 !~ /exclude = \[/ && $0 !~ /\]/) print $0}' typst.toml | sed 's/[",]//g' | sed 's/^[ \t]*//')
+exclude_patterns=$(awk '/exclude = \[/,/\]/ {if ($0 !~ /exclude = \[/ && $0 !~ /\]/) print $0}' typst.toml | sed 's/[",]//g' | sed 's/^[ \t]*//' | sed 's/\/$//')
 tar_args=$(while IFS= read -r line; do echo " --exclude=$line"; done <<<"$exclude_patterns")
 version=$(awk '/version =/ {print $3}' typst.toml | sed 's/"//g')
 tar_args+=" --exclude-vcs-ignores -czf ../algorithmic-$version.tar.gz ."
